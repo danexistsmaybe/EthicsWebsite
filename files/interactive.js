abf2f6bf65fb3624668ca433b0ffe65569fbfd28
Daniel Evans <danevans264@gmail.com>
2025-12-14 03:04:19 -0600

    Suffering

--- files/interactive.js
+++ files/interactive.js
@@ -71,12 +71,13 @@ function mainloop() {
 	// moment 4
 	setleft("moment4", (50 - .001*((pos - 300)**3))+"vw");
 	setheight("moment4", (30 + 50*hump(pos - 300)) + "px");
-	setopacity("moment-image4", 4 - .01*(pos - 300)*(pos-700));
+	setopacity("moment-image4", 4 - .01*(pos - 300)**2);
 	setleft("moment-text4", (50 - .001*((pos - 300)**3))+"vw");
 
 	// moment 5
 	setleft("moment5", (50 - .001*((pos - 400)**3))+"vw");
 	setheight("moment5", (30 + 50*hump(pos - 400)) + "px");
+	setopacity("moment-image5", 4 - .01*(pos - 400)*(pos - 700));
 	setleft("moment-text5", (50 - .001*((pos - 400)**3))+"vw");
 
 	// moment 5
